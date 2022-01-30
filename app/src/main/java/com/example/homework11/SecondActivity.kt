566ec0b7c4f4ce735376cb481664c704541a5383
Nyoigi <67091699+Nyoigi@users.noreply.github.com>
2022-01-30 15:41:32 +0300

    Initial Commit

--- app/src/main/java/com/example/homework11/SecondActivity.kt
+++ app/src/main/java/com/example/homework11/SecondActivity.kt
@@ -7,6 +7,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
--- app/src/main/java/com/example/homework11/ThirdActivity.kt
+++ app/src/main/java/com/example/homework11/ThirdActivity.kt
@@ -7,6 +7,7 @@ import kotlinx.android.synthetic.main.activity_second.*
 import kotlinx.android.synthetic.main.activity_third.*
 
 class ThirdActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
